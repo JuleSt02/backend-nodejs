0f622778a2af46e3c446278be49397c8db7ae91d
Nauel Gómez <ngomez@codiara.com>
2026-04-07 21:43:39 +0200

    Simulando peticiones lentas

--- examples/04-http/tasksRepository.js
+++ examples/04-http/tasksRepository.js
@@ -1,8 +1,14 @@
 import { readFile } from 'node:fs/promises';
+import { readFileSync } from 'node:fs';
+
+// TMP
+import { setTimeout as wait } from 'node:timers/promises'
 
 // TODO 
 export async function getTasks() {
+    // await wait(5000);
     const fileUrl = new URL('./tasks.json', import.meta.url);
     const fileContents = await readFile(fileUrl);
+    // const fileContents = await readFileSync(fileUrl);
     return JSON.parse(fileContents);
 }
