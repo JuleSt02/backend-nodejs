c330bda57016526d68abc28c054b955612f11a8a
Nauel Gómez <ngomez@codiara.com>
2026-04-09 22:18:19 +0200

    Repository 1: TODO

--- src/routes/pages-routes.js
+++ src/routes/pages-routes.js
@@ -1,38 +1,10 @@
 import express from 'express';
 
+import { homePageController } from '../controllers/pages-controllers.js';
+
 export const pagesRouter = express.Router();
 
-pagesRouter.get('/', (req, res, next) => {
-    const title = 'Server HTTP Básico';
-    const pendingTasks = 0;
-    const content = `
-        <h1>Server HTTP basico</h1>
-        <p>Este ejemplo ya respira web SSR: una ruta HTML, una lista HTML y una ruta de health.</p>
-    `;
-    res.send(`
-        <!doctype html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <link rel="stylesheet" href="/app.css">
-                    <link rel="icon" href="/icon.webp">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-                    <title>${title}</title>
-                </head>
-                <body style="">
-                    <nav class="container">
-                        <a href="/">Inicio</a>
-                        <a href="/tasks">Lista de tareas (${pendingTasks})</a>
-                        <a href="/health">Estado de la aplicación</a>
-                    </nav>
-                    ${content}
-
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-                </body>
-            </html>
-    `);
-});
+pagesRouter.get('/', homePageController);
 
 pagesRouter.get('/contact', (req, res, next) => {
     res.send('Need help? Contact us');
