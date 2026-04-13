b492d99547a0e47d07d0e3a81e4c789910d9c12c
Nauel Gómez <ngomez@codiara.com>
2026-04-13 19:46:44 +0200

    TODO views 1

--- src/controllers/pages-controllers.js
+++ src/controllers/pages-controllers.js
@@ -1,7 +1,10 @@
 import { countPendingTasks } from '../data/tasksRepository.js';
 
 export async function homePageController(req, res, next) {
-    res.render('index.html');
+    // TODO: Añade la variable que falta a la vista.
+    res.render('index.html', {
+        title: 'Server HTTP Básico',
+    });
     // TODO: Refactorizar
     return;
     res.locals.title = 'Server HTTP Básico';
